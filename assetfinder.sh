541ca388c9b24ffc7b0376d1f8e006cf53ddab13
Muhammad Haroon <mharoon1326@gmail.com>
2025-06-16 09:33:59 +0500

    Refactor formatting and remove unnecessary spaces in assetfinder.sh

--- assetfinder.sh
+++ assetfinder.sh
@@ -1,24 +1,20 @@
 #!/bin/bash
 url=$1
 
-
-if [ ! -d "$url" ] ; then
+if [ ! -d "$url" ]; then
 	mkdir $url
 fi
 
-
-if [ ! -d "$url/recon" ] ; then
+if [ ! -d "$url/recon" ]; then
 	mkdir $url/recon
 fi
 
-
 echo "[+] Harevesting Sub-Domains with assetfinder.................."
 
+assetfinder $url >>$url/recon/assets.txt
 
-assetfinder $url >> $url/recon/assets.txt
+cat $url/recon/assets.txt | grep $1 >>$url/recon/final.txt
 
-cat $url/recon/assets.txt | grep $1 >> $url/recon/final.txt
-
-rm  $url/recon/assets.txt
+rm $url/recon/assets.txt
 
 echo "[+] complete!"
